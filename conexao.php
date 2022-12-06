f08ebe73a97307cae68650f131cba60a3a424e24
WVsilva35 <116675986+WVsilva35@users.noreply.github.com>
2022-12-05 21:16:42 -0300

    é

--- conexao.php
+++ conexao.php
@@ -1,13 +1,9 @@
 <?php
-$servidor="localhost";
-$usuario="root";
-$senha="";
-$dbname="cadastros";
-
- $conexao=mysqli_connect($servidor, $usuario, $senha, $dbname);
- if(!$conexao) {
-    die("houve um erro: " .mysqli_connect_error());
- }
-
+# http://localhost/site-semjuv/connection.php
+define(" host "," localhost ");
+define(" usuário "," root ");
+define(" psw ","");
+define(" db "," apresentação de slides ");
+$ con =mysqli_connect(host, user, psw, db) or die(" erro no banco de dados de conexão ");
 
 ?>
